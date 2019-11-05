ed1380028a69d9b6520d9eea069f7c01ebcf8628
usuario <usuario@pc21-082>
2019-11-05 17:39:25 +0100

    Commit paso 2. Creacion clase calculadora y los metodos suma y resta que devuelven un entero.

---
+++ src/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class Calculadora {
+    
+    public static int suma(int a,int b){
+        return 8;
+    }
+    
+    public static int resta(int a,int b){
+        return 0;
+    }
+}
